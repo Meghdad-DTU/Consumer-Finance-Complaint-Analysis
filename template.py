5625d3cb2f9e0ec1bbed1a88fdf688610a1dfd11
meghdad <mehdizadeh.iust@gmail.com>
2023-10-09 11:00:12 -0400

    updating template.py by adding .gitkeep instead of main.yaml

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = "financeComplaint"
 
 list_of_files = [
-    ".github/workflows/main.yaml",
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/pipeline/__init__.py",    
