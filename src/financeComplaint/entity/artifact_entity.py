ab2b946fea9fc07f0f2d83fa59cc712dfc47224c
meghdad <mehdizadeh.iust@gmail.com>
2023-10-11 14:58:06 -0400

    creating artifacts_entity.py and completing for data ingestion stage

--- src/financeComplaint/entity/artifact_entity.py
+++ src/financeComplaint/entity/artifact_entity.py
@@ -4,5 +4,6 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataIngestionArtifact:
     feature_store_file_path: Path
-    metadata_file_path: Path
     downloaded_dir: Path
+    metadata_file_path: Path
+    
